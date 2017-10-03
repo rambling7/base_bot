4c13b2574b918f80efd48084f700801cb3a370bd
rambling7 <29454244+rambling7@users.noreply.github.com>
2017-10-03 15:18:55 +0300

    new file bot_class and main

---
+++ bot_class.py
@@ -0,0 +1,48 @@
+import requests
+import random
+
+
+class BaseBot:
+	
+	def __init__(self, token):
+		self.token = token
+		self.api_url = 'https://api.telegram.org/bot%s/' % token
+
+	def get_updates(self, offset=None, timeout=30):
+		method = 'getUpdates'
+		params = {'timeout': timeout, 'offset': offset}
+		resp = requests.get(self.api_url + method, params)
+		result_json = resp.json()['result']
+		return result_json
+		
+	def send_message(self, chat_id, text):
+		params = {'chat_id': chat_id, 'text': text}
+		method = 'sendMessage'
+		resp = requests.post(self.api_url + method, params)
+		return resp
+
+	def get_last_update(self):
+		get_result = self.get_updates()
+		if len(get_result) > 0:
+			last_update = get_result[-1]
+		else:
+			last_update = None
+		return last_update
+		
+class SimpleResponseBot(BaseBot):
+	
+	def auto_response(self, response_id):
+		response_base = (	'встрой поисковики', 
+							'взбодрись и продолжай', 
+							'читай книги',
+							'не тупи, делай',
+							'ты молодец',
+							'изучай апи',
+							'изучай линукс',
+							'изучай питон',
+							'решай задачи',
+							'доделай меня',
+							'изучай машинное обучение')
+		self.send_message(response_id, random.choice(response_base))
+	
+	
